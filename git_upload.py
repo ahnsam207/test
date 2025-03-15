51086362be730b67f4e6811cdf10bd12cc442698
ahnsam207 <2ok25@daum.net>
2025-03-15 21:43:58 +0900

    Update git_upload.py

--- git_upload.py
+++ git_upload.py
@@ -3,7 +3,7 @@ import requests
 import base64
 
 # GitHub 정보
-GITHUB_REPO = "ahnsam207/test"
+GITHUB_REPO = st.secrets["repo"]
 GITHUB_TOKEN =  st.secrets["git_token"]   # 개인 액세스 토큰 입력
 BRANCH = "main"  # 사용할 브랜치
 
